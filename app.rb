1a89b9ed744a36fa07dfd4c633022f27b2d5bd40
georgie-porgie-pudding-pie <gkobalya@chicagobooth.edu>
2025-04-09 01:59:39 +0000

    4.8.2059

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
+require "json"
+require "dotenv/load"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @exchange_rate_key = ENV.fetch("EXCHANGE_RATE_KEY")
+  @exchange_rate_list_url = "https://api.exchangerate.host/list?access_key=" + @exchange_rate_key
+  @raw_response = HTTP.get(@exchange_rate_list_url)
+  @parsed_response = JSON.parse(@raw_response)
+  @currencies = @parsed_response.fetch("currencies").keys
+  erb(:home)
+end
+
+get("/:first_currency") do
+  @first_curr = params.fetch("first_currency").to_s
+  erb(:flexible)
 end
